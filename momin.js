d7c034269a1133ad309a27aeb8e054a4a68a1774
Muhammad Khalad <khaladjaan09@gmail.com>
2023-07-24 21:13:20 +0500

    Momin

--- momin.js
+++ momin.js
@@ -1,13 +1,3 @@
-
-document.getElementById("bars").addEventListener("click",abc);
-function abc(){
-let a=document.getElementById("menu");
-if(a.style.display=="none"){
-    a.style.display="block";
-}
-else if (a.style.display="block"){
-    a.style.display="none";
-}
-}
-
-
+document.getElementById("bars").addEventListener("click" , function(){
+    document.getElementById("menu").classList.toggle("abc");
+})
